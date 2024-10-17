2ad0b76d88c43abd9ffabc509848745d9049eb18
code-manish <code.manish94@gmail.com>
2024-10-18 00:47:40 +0530

    added spring security

--- securitydemo/src/main/java/com/spring/securitydemo/SpringSecurityDemoApplication.java
+++ securitydemo/src/main/java/com/spring/securitydemo/SpringSecurityDemoApplication.java
@@ -2,8 +2,10 @@ package com.spring.securitydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.spring.securitydemo")
 public class SpringSecurityDemoApplication {
 
 	public static void main(String[] args) {
